6a0c0b2df961294084f46c12f084951c402e0fc6
Shubh Saxena <shubhsaxena447@gmail.com>
2025-02-17 22:39:46 +0530

    fix: Update routing to redirect to dashboard on root path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import store from './store/store.js'
 import {Provider} from 'react-redux'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider} from 'react-router-dom'
 import {Login,SignUp,Dashboard, Layout} from './pages/index.js'
 import {ProtectedRoute} from './components/index.js'
 import './main.css'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout/>}>
-    <Route path='' element={<ProtectedRoute element={<Dashboard/>}/>}/>
+    <Route path='' element={<Navigate to="/dashboard" replace />}/>
     <Route path='login' element={<Login/>}/>
     <Route path='signup' element={<SignUp/>}/>
     <Route path='dashboard' element={<ProtectedRoute element={<Dashboard/>}/>}/>
